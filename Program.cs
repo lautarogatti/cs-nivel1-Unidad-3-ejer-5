4b3200c989c1ad76a56df8dc8afd815f7f99f5b4
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 02:26:11 -0300

    subida al repo

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Hacer un programa para ingresar cuatro números
+             y luego mostrar por pantalla cuáles son mayores a 100*/
+
+             int n1, n2, n3, n4;
+            Console.WriteLine("Ingrese el primer numero");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer numero");
+            n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el cuarto numero");
+            n4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Los numero mayores a 100 son: ");
+            if(n1 > 100){
+                Console.WriteLine(n1);
+            }
+            if(n2 > 100){
+                Console.WriteLine(n2);
+            }
+            if(n3 > 100){
+                Console.WriteLine(n3);
+            }
+            if(n4 > 100){
+                Console.WriteLine(n4);
+            }
+
+        }
+    }
+}
